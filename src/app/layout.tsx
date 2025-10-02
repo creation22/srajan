67b582653b07ba787af6ef18c368d2c7be2a7970
creation <creation2224@gmail.com>
2025-10-02 18:01:35 +0530

    error fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,13 +55,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Analytics/>
+      
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
           fontSans.variable
         )}
       >
+        <Analytics/>
         <ThemeProvider attribute="class" defaultTheme="light">
           <TooltipProvider delayDuration={0}>
             {children}
